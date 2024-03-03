5350f84f3abd94380acab23984059b939aba4f2e
wwu <wucody59@gmail.com>
2024-03-02 22:31:27 -0500

    健身房之王

--- Assignment4/src/Company.java
+++ Assignment4/src/Company.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Company {
+public class Company implements CompanyInterface{
+  String symbol;
+
+  Stock stockInfo;
+  public Company(String symbol, Stock stockInfo ){
+    this.symbol = symbol;
+    this.stockInfo = stockInfo;
+
+  }
+  /**
+   * Get the company symbol.
+   * eg, "GOOG" for Google.
+   *
+   * @return the company symbol string.
+   */
+  @Override
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public Stock getStockInfo(){
+    return stockInfo;
+  }
 }
--- Assignment4/src/CompanyInterface.java
+++ Assignment4/src/CompanyInterface.java
@@ -1,3 +1,10 @@
 public interface CompanyInterface {
+  /**
+   * Get the company symbol.
+   * eg, "GOOG" for Google.
+   * @return the company symbol string.
+   */
+  public String getSymbol();
+
 
 }
