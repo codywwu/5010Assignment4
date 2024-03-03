6fd904e6ca599a0e8fd11cf2d104d43ff82ee2d8
wwu <wucody59@gmail.com>
2024-03-03 16:22:02 -0500

    Adding getters for stock , user and company

--- Assignment4/src/StockController.java
+++ Assignment4/src/StockController.java
@@ -1,13 +1,29 @@
-public class Controller {
+public class StockController {
   private Stock model;
   private Stock view;
 
-  public Controller(Stock model, Stock view) {
+  public StockController(Stock model, Stock view) {
     this.model = model;
     this.view = view;
   }
 
   public void setStockCompanyName(String name ){
     model.setCompanyName(name);
+  }
+
+  public void setStockVolum(long volume){
+    model.setVolume(volume);
+  }
+
+  public void setStockLocalHigh(long localHigh) {
+    model.setLocalHigh(localHigh);
+  }
+
+  public void setStockLocalLow(long localLow) {
+    model.setLocalLow(localLow);
+  }
+
+  public void setStockTimeStamp(String timeStamp){
+    model.setTimeStamp(timeStamp);
   }
 }
