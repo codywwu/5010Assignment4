835fa28b5ef0bde3a5c5fc71c7e6c4b07f657cc6
wanjing27 <wanjing0227@gmail.com>
2024-03-12 22:01:26 -0400

    clean up code

--- Assignment4/src/models/Model.java
+++ Assignment4/src/models/Model.java
@@ -1,13 +1,16 @@
 package models;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class Model {
-  private final XMLDatabase xmlDatabase;
+  private XMLDatabase xmlDatabase;
   private List<Portfolio> userPortfolios;
   private User user;
+
+  private Portfolio portfolio;
 
   // Constructor
   public Model() {
@@ -18,23 +21,26 @@ public class Model {
     return userPortfolios.isEmpty();
   }
 
-  public XMLDatabase newXML(){
-    return new XMLDatabase();
-  }
   public Boolean checkInputName(String name){
     user = new User(name,0);
     return XMLDatabase.checkName(name);
   }
-
-
-  public Stock createStock(String companySymbol,long userShared) {
-    return new Stock(companySymbol,userShared);
-  }
-
   public User creatUser(String username,float buyingPower){
     User user = new User(username,buyingPower);
     xmlDatabase.addUser(username);
     return user;
+  }
+
+  public void addPortfolioUser(){
+    user.addPortfolio(portfolio);
+  }
+
+  public String getUserName(){
+    return user.getUserName();
+  }
+
+  public ArrayList<Portfolio> getPList(){
+    return user.getPortfolioList();
   }
 
   public List<Portfolio> getUserPortfolios() {
@@ -55,8 +61,48 @@ public class Model {
     return false; // Portfolio name not found
   }
 
-  public Portfolio readImport(String fileName){
-    return xmlDatabase.readImportedFile(fileName);
+  public Portfolio getPortfolio(){
+    return portfolio;
+  }
+
+  public Stock createStock(String companySymbol,long userShared) {
+    return new Stock(companySymbol,userShared);
+  }
+
+  public void newXML(){
+    xmlDatabase=new XMLDatabase();
+  }
+
+  public Boolean dataCheckExistInXML(Stock stock,String date){
+    return xmlDatabase.isDateExistInXML(stock.getCompanyName(), date);
+  }
+
+  public String getDatahigh(){
+    return XMLDatabase.highStock.trim();
+  }
+
+  public String getDataLow(){
+    return XMLDatabase.lowStock.trim();
+  }
+
+  public void createPortfolio(String name){
+    portfolio= new Portfolio(name);
+  }
+
+  public void addStockPort(Stock stock){
+    portfolio.addStock(stock);
+  }
+
+  public void addPToXML(){
+    xmlDatabase.addPortfolioXML(getUserName(),portfolio.name,portfolio);
+  }
+
+  public void addCompanyXML(String c){
+    xmlDatabase.createXMLbyCompanyInfo(c);
+  }
+
+  public void readImport(String fileName){
+    portfolio=xmlDatabase.readImportedFile(fileName);
   }
 
   public Boolean checkFileExists(String inFile){
@@ -76,9 +122,7 @@ public class Model {
     return false;
   }
 
-  public Portfolio createPortfolio(String name){
-    return new Portfolio(name);
-  }
+
 
 
   public static void displayPortfolioValueByGivenDate(List<Portfolio> portfolios, String givenDate,String portfolioName){
